4916c07012ee0fa751f8c22037e38080a4cc3f41
Andre Barbosa <andre.barbosa@quintoandar.com.br>
2020-09-05 16:13:20 -0300

    update aula3

--- scripts/serve.py
+++ scripts/serve.py
@@ -4,6 +4,7 @@ import sys
 
 import numpy as np
 import pandas as pd
+from flask import Flask, jsonify, request
 from joblib import load
 from sklearn.pipeline import Pipeline
 
@@ -18,6 +19,7 @@ logging.basicConfig(
     datefmt="%m/%d/%Y %I:%M:%S %p",
 )
 logger = logging.getLogger(__name__)
+app = Flask("producao-modelo")
 
 
 def load_model(pipeline_dir: str) -> Pipeline:
@@ -28,31 +30,28 @@ def load_model(pipeline_dir: str) -> Pipeline:
 def run_pipeline(pipeline: Pipeline, **features: dict) -> float:
     diff = set(NUMERICAL_FEATURES) - features.keys()
     if len(diff) != 0:
-        logging.error(
-            "Please review your passed features, the different set" f"was set by {diff}"
-        )
-        sys.exit("Check the feature names that you have passed!")
         return -1
     datapoint = pd.DataFrame.from_dict(features, orient="index").T
     datapoint = datapoint[NUMERICAL_FEATURES]  # garantir ordem!
     result = pipeline.predict(datapoint)[0][0]
     logging.info(f"This house should have {result:.2f} dollars as price")
-    return 1
+    return result
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    full_pipe = load_model(PIPELINE_DIR)
+    json_ = request.json
+    result = run_pipeline(full_pipe, **json_)
+    if result == -1:
+        logging.error("Please review your passed features")
+        result = None
+    return jsonify(result)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "Hello world"
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Define features of a datapoint")
-    parser.add_argument("--lot_area", type=float, help="House Area", required=True)
-    parser.add_argument("--year", type=int, help="House Built Year", required=True)
-    parser.add_argument(
-        "--quality", type=int, help="Overall Quality House", required=True
-    )
-
-    args = parser.parse_args()
-    features = {
-        "LotArea": args.lot_area,
-        "YearBuilt": args.year,
-        "OverallQual": args.quality,
-    }
-    run_pipeline(load_model(PIPELINE_DIR), **features)
+    app.run(host="0.0.0.0", port=5000)
